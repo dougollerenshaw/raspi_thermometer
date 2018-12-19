c8ce052b4ff14f6efbffa2f8939e20e38b03cd4e
dougollerenshaw <d.ollerenshaw@gmail.com>
2018-12-18 22:33:49 -0800

    dht11 update

--- raspi_thermometer/command_line_print.py
+++ raspi_thermometer/command_line_print.py
@@ -2,8 +2,14 @@
 import sys
 import Adafruit_DHT
 
+# Type of sensor, can be Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+DHT_TYPE = Adafruit_DHT.DHT11
+
+# Example of sensor connected to Raspberry Pi pin 23
+DHT_PIN  = 4
+
 while True:
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE,DHT_PIN)
 
     print 'Temp: {0:0.2f} C  Humidity: {1:0.2f} %'.format(temperature, humidity)
